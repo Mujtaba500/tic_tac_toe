56726e6f97a20063bea3d3b8eabfad634d2fa782
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2025-08-31 14:42:46 +0500

    project setup

--- servers/auth_service/src/app.ts
+++ servers/auth_service/src/app.ts
@@ -1,7 +1,38 @@
-import { log } from "console";
+import { log } from 'console';
+import express from 'express';
+import 'dotenv/config';
+import helmet from 'helmet';
+import chalk from 'chalk';
+import cors from 'cors';
+import http from 'http';
+import createDebug from 'debug';
 
-const sayHello = (name: string) => {
-  log(name);
-};
+const port = process.env.PORT;
 
-sayHello("yolo");
+const app = express();
+const debug = createDebug('http');
+
+debug('booting Express server');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(helmet());
+
+app.get('/', (req: express.Request, res) => {
+  debug(req.method + ' ' + req.url);
+  res.json({
+    message: 'API live'
+  });
+});
+
+app.get('/health', (req, res) => {
+  // db authenticate
+});
+
+const server: http.Server = http.createServer(app);
+
+server.listen(port, () => {
+  log(chalk.green('✓'), `Server listening on port ${port}`);
+});
